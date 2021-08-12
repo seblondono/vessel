b2dccef076d8bfecf80d82147eb5da1e88912afc
seblondono <sebastianlondonom@gmail.com>
2021-08-12 12:53:25 +0200

    catch a tailwind
    * add tailwind CSS utilities to speedup or vessel and sail with style

--- vessel/src/App.tsx
+++ vessel/src/App.tsx
@@ -1,11 +1,8 @@
-import React from 'react'
-import './App.css'
-
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1>Vessel</h1>
+    <div className='w-screen h-screen bg-yellow-500'>
+      <header className='h-full flex justify-center items-center'>
+        <div className='text-7xl text-white'>Vessel</div>
       </header>
     </div>
   )
