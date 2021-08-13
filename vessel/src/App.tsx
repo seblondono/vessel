b4b976ba6653f852c68c8bf90027b328ff6f11f7
seblondono <sebastianlondonom@gmail.com>
2021-08-13 20:29:11 +0200

    turn the lighthouse on
    * run our lighthouse script

--- vessel/src/App.tsx
+++ vessel/src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <div className='w-screen h-screen bg-yellow-500'>
-      <header className='h-full flex justify-center items-center'>
-        <div className='text-7xl text-white'>Vessel</div>
+    <div className="w-screen h-screen bg-yellow-500">
+      <header className="h-full flex justify-center items-center">
+        <div className="text-7xl text-white">Vessel</div>
       </header>
     </div>
   )
