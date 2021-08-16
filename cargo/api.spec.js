cd6780352e3ac831e2ac8b7406b77a6ac5b434e4
seblondono <sebastianlondonom@gmail.com>
2021-08-16 16:19:17 +0200

    turn the cargo bay lighthouse on
    * run the lighthouse script

--- cargo/api.spec.js
+++ cargo/api.spec.js
@@ -1,25 +1,20 @@
-import assert from 'assert';
-import { members, absences } from './api';
+import assert from 'assert'
+import {members, absences} from './api'
 
-const everyItemContainsKey = (key) => (collection) =>
-  collection.forEach((item) => assert(Object.keys(item).includes(key)));
+const everyItemContainsKey = key => collection =>
+  collection.forEach(item => assert(Object.keys(item).includes(key)))
 
 describe('members', () => {
   describe('every member has key', () => {
-    [
-      'id',
-      'name',
-      'userId',
-      'image',
-    ].forEach((key) => {
-      it(key, () => members().then(everyItemContainsKey(key)));
-    });
-  });
-});
+    ;['id', 'name', 'userId', 'image'].forEach(key => {
+      it(key, () => members().then(everyItemContainsKey(key)))
+    })
+  })
+})
 
 describe('absences', () => {
   describe('every absence has key', () => {
-    [
+    ;[
       'admitterNote',
       'confirmedAt',
       'createdAt',
@@ -31,9 +26,8 @@ describe('absences', () => {
       'startDate',
       'type',
       'userId',
-    ].forEach((key) => {
-      it(key, () => absences().then(everyItemContainsKey(key)));
-    });
-  });
-});
-
+    ].forEach(key => {
+      it(key, () => absences().then(everyItemContainsKey(key)))
+    })
+  })
+})
--- cargo/index.ts
+++ cargo/index.ts
@@ -1,14 +1,14 @@
-import express, { Application } from 'express'
+import express, {Application} from 'express'
 
 const app: Application = express()
 const port = 5000
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', async (_req, res) => {
   return res.status(200).send({
-    message: 'Welcome to the vessel cargo bay'
+    message: 'Welcome to the vessel cargo bay',
   })
 })
 
@@ -16,6 +16,6 @@ try {
   app.listen(port, () => {
     console.log(`Listening on port ${port}`)
   })
-} catch(error) {
+} catch (error) {
   console.error(`Error trying to listen on port ${port}: ${error.message}`)
 }
