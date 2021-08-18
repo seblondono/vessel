c46d0bff83c0c0ed1e040cbb6dcaed85e19e6c61
seblondono <sebastianlondonom@gmail.com>
2021-08-18 23:27:35 +0200

    add crew absences table
    * table has sticky headers on scroll
    * member and admitter notes are truncated but full content can be seen as the cells titles when user hovers over the cells

--- vessel/src/components/absenceManager/AbsenceManager.tsx
+++ vessel/src/components/absenceManager/AbsenceManager.tsx
@@ -5,9 +5,31 @@ const AbsenceManager: FC = () => {
   const absences = useQueryAbsences()
 
   return (
-    <div className='h-full flex flex-col justify-center items-center'>
-      <div className='text-5xl font-bold'>Absence Manager</div>
-      <p>{JSON.stringify(absences.data)}</p>
+    <div className='h-full overflow-auto p-8'>
+      <table className='relative w-full table-fixed border-collapse border'>
+        <thead>
+        <tr>
+          <th className='sticky top-table-sticky text-left bg-yellow-100 p-1 w-1/5 font-medium border-t border-b'>Member Name</th>
+          <th className='sticky top-table-sticky text-left bg-yellow-100 p-1 w-1/5 font-medium border-t border-b'>Type</th>
+          <th className='sticky top-table-sticky text-left bg-yellow-100 p-1 w-1/5 font-medium border-t border-b'>Period</th>
+          <th className='sticky top-table-sticky text-left bg-yellow-100 p-1 w-1/5 font-medium border-t border-b'>Member Note</th>
+          <th className='sticky top-table-sticky text-left bg-yellow-100 p-1 w-1/5 font-medium border-t border-b'>Admitter Note</th>
+        </tr>
+        </thead>
+        <tbody className='divide-y'>
+        {absences.data?.map((it) => {
+          return (
+            <tr key={it.id}>
+              <td className='p-1'>{it.userId}</td>
+              <td className='p-1'>{it.type}</td>
+              <td className='p-1'>{`${it.startDate} - ${it.endDate}`}</td>
+              <td className='p-1 truncate' title={it.memberNote}>{it.memberNote}</td>
+              <td className='p-1 truncate' title={it.admitterNote}>{it.admitterNote}</td>
+            </tr>
+          )
+        })}
+        </tbody>
+      </table>
     </div>
   )
 }
