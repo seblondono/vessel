888c9990263833324ac3b09462d94e5ed8a3d422
seblondono <sebastianlondonom@gmail.com>
2021-08-18 13:12:16 +0200

    add endpoints for getting all absences or an absence by id

--- cargo/src/index.ts
+++ cargo/src/index.ts
@@ -1,15 +1,29 @@
-import express, {Application} from 'express'
+import express, { Application } from 'express'
+import { absences } from './api'
+import { isValue } from './util/typeGuardUtil'
 
 const app: Application = express()
 const port = 5000
 
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', async (_req, res) => {
-  return res.status(200).send({
-    message: 'Welcome to the vessel cargo bay',
-  })
+app.get('/absences', async (_req, res) => {
+  const crewAbsences = await absences()
+  return res.status(200).send(crewAbsences)
+})
+
+app.get('/absence/:id', async (req, res) => {
+  const crewAbsences = await absences()
+  const absence = crewAbsences.find((it) => it.id === Number(req.params.id))
+
+  if (!isValue(absence)) {
+    return res.status(404).send({
+      message: `Cannot find absence with id ${req.params.id}`,
+    })
+  }
+
+  return res.status(200).send(absence)
 })
 
 try {
